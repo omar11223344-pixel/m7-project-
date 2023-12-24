e1eee3bce75de6e5fb38e51a1675335f030bc1d0
omar11223344-pixel <154749348+omar11223344-pixel@users.noreply.github.com>
2023-12-24 20:00:53 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,9 @@
+document.getElementById('b1').addEventListener('click',function(){
+    window.location.href=('login.html')})
+    document.getElementById('send').addEventListener('click',function(){
+    window.location.href=('reservation.html')   
+    })
+    document.getElementById('follow').addEventListener('click',function(){
+    window.location.href=('offers.html')
+    })
+    
